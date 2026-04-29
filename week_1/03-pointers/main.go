eca83872e98d81472b66df38b34a1089a9fb2fa7
Javascriptdev347 <javascriptdev347@gmail.com>
2026-04-29 18:05:48 +0500

    pointer

--- week_1/03-pointers/main.go
+++ week_1/03-pointers/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Pointer ")
+
+	a := makeMultiplier(3)
+	for i := 1; i <= 9; i++ {
+		fmt.Println(a(i))
+	}
+
+}
+
+func makeMultiplier(factor int) func(int) int {
+	countOfCalls := 0
+
+	return func(i int) int {
+		countOfCalls += 1
+		bonus := 0
+		if countOfCalls%3 == 0 {
+			bonus = 100
+		}
+
+		return factor*i + bonus
+	}
 }
