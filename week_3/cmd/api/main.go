b13c4ae42a8216b5ce5c20c59d3348a14b1b1c9a
Javascriptdev347 <javascriptdev347@gmail.com>
2026-07-13 14:03:42 +0500

    middleware

--- week_3/cmd/api/main.go
+++ week_3/cmd/api/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello world ")
-		fmt.Println("R:", r.Method)
-		fmt.Println("W:", w)
-	})
+	mux := http.NewServeMux()
+	mux.HandleFunc("/all", getUsersHandler)
+	mux.HandleFunc("/create", createUserHandler)
+	mux.HandleFunc("/users/{id}", getUserByIDHandler)
 
-	http.ListenAndServe(":8080", nil)
+	wrappedMux := loggingMiddleware(mux)
+	http.ListenAndServe(":8080", wrappedMux)
+}
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "List of all users")
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "User created")
+}
+
+func getUserByIDHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id") // Go 1.22+ feature
+	fmt.Fprintf(w, "User ID: %s\n", id)
+}
+
+// middleware
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r) // call the wrapped handler
+		log.Printf("%s %s took %v", r.Method, r.URL.Path, time.Since(start))
+	})
 }
