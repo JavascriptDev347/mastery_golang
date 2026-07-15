42970c310c2da6d8acba92f5b9944a41be335cf4
Javascriptdev347 <javascriptdev347@gmail.com>
2026-07-15 18:00:28 +0500

    notes

--- week_3/cmd/api/main.go
+++ week_3/cmd/api/main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.Run(":8080")
 
 }
