f6f997a158a88c4a8d0a1d6a727dcf23e6ac4949
Javascriptdev347 <javascriptdev347@gmail.com>
2026-09-22 17:55:38 +0500

    learn: http

---
+++ foundation/http/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	r := bufio.NewReader(conn)
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			return
+		}
+		fmt.Println("%q\n", line)
+		if line == "\r\n" {
+			break
+		}
+	}
+
+	body := "salom\n"
+	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
+}
